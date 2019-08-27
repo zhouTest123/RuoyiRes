da87664e0585372759ceb41e9264745e3afb7d34
zhouTest123 <45376043+zhouTest123@users.noreply.github.com>
2019-08-27 21:33:16 +0800

    Update SysPostController.java

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysPostController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysPostController.java
@@ -5,14 +5,12 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.constant.UserConstants;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
@@ -22,11 +20,15 @@ import com.ruoyi.framework.util.ShiroUtils;
 import com.ruoyi.system.domain.SysPost;
 import com.ruoyi.system.service.ISysPostService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * 岗位信息操作处理
  * 
  * @author ruoyi
  */
+@Api("岗位信息操作处理")
 @Controller
 @RequestMapping("/system/post")
 public class SysPostController extends BaseController
@@ -43,6 +45,7 @@ public class SysPostController extends BaseController
         return prefix + "/post";
     }
 
+    @ApiOperation("获取岗位信息列表")
     @RequiresPermissions("system:post:list")
     @PostMapping("/list")
     @ResponseBody
@@ -53,6 +56,7 @@ public class SysPostController extends BaseController
         return getDataTable(list);
     }
 
+    @ApiOperation("岗位信息导出")
     @Log(title = "岗位管理", businessType = BusinessType.EXPORT)
     @RequiresPermissions("system:post:export")
     @PostMapping("/export")
@@ -64,6 +68,7 @@ public class SysPostController extends BaseController
         return util.exportExcel(list, "岗位数据");
     }
 
+    @ApiOperation("岗位信息删除")
     @RequiresPermissions("system:post:remove")
     @Log(title = "岗位管理", businessType = BusinessType.DELETE)
     @PostMapping("/remove")
@@ -92,20 +97,13 @@ public class SysPostController extends BaseController
     /**
      * 新增保存岗位
      */
+    @ApiOperation("新增岗位信息")
     @RequiresPermissions("system:post:add")
     @Log(title = "岗位管理", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(@Validated SysPost post)
+    public AjaxResult addSave(SysPost post)
     {
-        if (UserConstants.POST_NAME_NOT_UNIQUE.equals(postService.checkPostNameUnique(post)))
-        {
-            return error("新增岗位'" + post.getPostName() + "'失败，岗位名称已存在");
-        }
-        else if (UserConstants.POST_CODE_NOT_UNIQUE.equals(postService.checkPostCodeUnique(post)))
-        {
-            return error("新增岗位'" + post.getPostName() + "'失败，岗位编码已存在");
-        }
         post.setCreateBy(ShiroUtils.getLoginName());
         return toAjax(postService.insertPost(post));
     }
@@ -123,20 +121,13 @@ public class SysPostController extends BaseController
     /**
      * 修改保存岗位
      */
+    @ApiOperation("修改岗位信息")
     @RequiresPermissions("system:post:edit")
     @Log(title = "岗位管理", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(@Validated SysPost post)
+    public AjaxResult editSave(SysPost post)
     {
-        if (UserConstants.POST_NAME_NOT_UNIQUE.equals(postService.checkPostNameUnique(post)))
-        {
-            return error("修改岗位'" + post.getPostName() + "'失败，岗位名称已存在");
-        }
-        else if (UserConstants.POST_CODE_NOT_UNIQUE.equals(postService.checkPostCodeUnique(post)))
-        {
-            return error("修改岗位'" + post.getPostName() + "'失败，岗位编码已存在");
-        }
         post.setUpdateBy(ShiroUtils.getLoginName());
         return toAjax(postService.updatePost(post));
     }
@@ -144,6 +135,7 @@ public class SysPostController extends BaseController
     /**
      * 校验岗位名称
      */
+    @ApiOperation("校验岗位名称")
     @PostMapping("/checkPostNameUnique")
     @ResponseBody
     public String checkPostNameUnique(SysPost post)
@@ -154,6 +146,7 @@ public class SysPostController extends BaseController
     /**
      * 校验岗位编码
      */
+    @ApiOperation("校验岗位编码")
     @PostMapping("/checkPostCodeUnique")
     @ResponseBody
     public String checkPostCodeUnique(SysPost post)
