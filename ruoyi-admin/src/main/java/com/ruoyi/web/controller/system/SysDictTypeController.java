269a3af3c99b0e07145206550e021a12a5dde12d
zhouTest123 <45376043+zhouTest123@users.noreply.github.com>
2019-08-27 21:27:29 +0800

    Update SysDictTypeController.java
    Add API Interface

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysDictTypeController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysDictTypeController.java
@@ -5,17 +5,14 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.constant.UserConstants;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.domain.Ztree;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.utils.poi.ExcelUtil;
@@ -23,11 +20,15 @@ import com.ruoyi.framework.util.ShiroUtils;
 import com.ruoyi.system.domain.SysDictType;
 import com.ruoyi.system.service.ISysDictTypeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * 数据字典信息
  * 
  * @author ruoyi
  */
+@Api("数据字典类型")
 @Controller
 @RequestMapping("/system/dict")
 public class SysDictTypeController extends BaseController
@@ -44,6 +45,7 @@ public class SysDictTypeController extends BaseController
         return prefix + "/type";
     }
 
+    @ApiOperation("获取数据字典类型列表")
     @PostMapping("/list")
     @RequiresPermissions("system:dict:list")
     @ResponseBody
@@ -54,6 +56,7 @@ public class SysDictTypeController extends BaseController
         return getDataTable(list);
     }
 
+    @ApiOperation("字典类型")
     @Log(title = "字典类型", businessType = BusinessType.EXPORT)
     @RequiresPermissions("system:dict:export")
     @PostMapping("/export")
@@ -78,16 +81,13 @@ public class SysDictTypeController extends BaseController
     /**
      * 新增保存字典类型
      */
+    @ApiOperation("新增保存字典类型")
     @Log(title = "字典类型", businessType = BusinessType.INSERT)
     @RequiresPermissions("system:dict:add")
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(@Validated SysDictType dict)
+    public AjaxResult addSave(SysDictType dict)
     {
-        if (UserConstants.DICT_TYPE_NOT_UNIQUE.equals(dictTypeService.checkDictTypeUnique(dict)))
-        {
-            return error("新增字典'" + dict.getDictName() + "'失败，字典类型已存在");
-        }
         dict.setCreateBy(ShiroUtils.getLoginName());
         return toAjax(dictTypeService.insertDictType(dict));
     }
@@ -105,20 +105,18 @@ public class SysDictTypeController extends BaseController
     /**
      * 修改保存字典类型
      */
+    @ApiOperation("修改保存字典类型")
     @Log(title = "字典类型", businessType = BusinessType.UPDATE)
     @RequiresPermissions("system:dict:edit")
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(@Validated SysDictType dict)
+    public AjaxResult editSave(SysDictType dict)
     {
-        if (UserConstants.DICT_TYPE_NOT_UNIQUE.equals(dictTypeService.checkDictTypeUnique(dict)))
-        {
-            return error("修改字典'" + dict.getDictName() + "'失败，字典类型已存在");
-        }
         dict.setUpdateBy(ShiroUtils.getLoginName());
         return toAjax(dictTypeService.updateDictType(dict));
     }
 
+    @ApiOperation("删除字典类型")
     @Log(title = "字典类型", businessType = BusinessType.DELETE)
     @RequiresPermissions("system:dict:remove")
     @PostMapping("/remove")
@@ -138,6 +136,7 @@ public class SysDictTypeController extends BaseController
     /**
      * 查询字典详细
      */
+    @ApiOperation("查询字典详细")
     @RequiresPermissions("system:dict:list")
     @GetMapping("/detail/{dictId}")
     public String detail(@PathVariable("dictId") Long dictId, ModelMap mmap)
@@ -150,33 +149,11 @@ public class SysDictTypeController extends BaseController
     /**
      * 校验字典类型
      */
+    @ApiOperation("校验字典类型")
     @PostMapping("/checkDictTypeUnique")
     @ResponseBody
     public String checkDictTypeUnique(SysDictType dictType)
     {
         return dictTypeService.checkDictTypeUnique(dictType);
-    }
-
-    /**
-     * 选择字典树
-     */
-    @GetMapping("/selectDictTree/{columnId}/{dictType}")
-    public String selectDeptTree(@PathVariable("columnId") Long columnId, @PathVariable("dictType") String dictType,
-            ModelMap mmap)
-    {
-        mmap.put("columnId", columnId);
-        mmap.put("dict", dictTypeService.selectDictTypeByType(dictType));
-        return prefix + "/tree";
-    }
-
-    /**
-     * 加载字典列表树
-     */
-    @GetMapping("/treeData")
-    @ResponseBody
-    public List<Ztree> treeData()
-    {
-        List<Ztree> ztrees = dictTypeService.selectDictTree(new SysDictType());
-        return ztrees;
     }
 }
